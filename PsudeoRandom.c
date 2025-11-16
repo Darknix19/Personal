ba113d91f37c3eb743835c044c2485cd24fcb229
Khunais Baig <khunaisbaig29@gmail.com>
2025-11-16 10:33:43 +0500

    Create PsudeoRandom.c

---
+++ PsudeoRandom.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    srand(time(NULL));
+
+    int min = 1;
+    int max = 100;
+
+    for (int i = 0; i < 10; i++)
+    {
+        int randomNum1 = (rand() % (max - min +1)) + min;
+        int randomNum2 = (rand() % (max - min +1)) + min;
+        int randomNum3 = (rand() % (max - min +1)) + min;
+
+        printf("%d %d %d\n", randomNum1, randomNum2, randomNum3);
+    }
+    
+    return 0;
+}
